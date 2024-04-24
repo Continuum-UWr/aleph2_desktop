b7e6520624dbaaf2770c21c6b8ff36621b52615f
Błażej Sowa <bsowa123@gmail.com>
2024-04-25 00:35:33 +0200

    Fix aleph2_gui nix

--- aleph2_gui/default.nix
+++ aleph2_gui/default.nix
@@ -1,5 +1,6 @@
 { buildRosPackage, rclpy, ament-index-python, python3Packages, rqt-gui
-, rqt-gui-py, input-manager }: {
+, rqt-gui-py, input-manager }:
+buildRosPackage {
   pname = "aleph2-gui";
   version = "0.0.0";
 
