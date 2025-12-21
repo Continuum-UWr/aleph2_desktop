3dee2f24363e4d3731e77ec9ee11ec0be634a531
Błażej Sowa <bsowa123@gmail.com>
2025-12-21 17:34:09 +0100

    Update flake.lock and flake.nix for jazzy branch references and formatting

--- flake.nix
+++ flake.nix
@@ -3,33 +3,39 @@
     nixpkgs.follows = "aleph2-common/nixpkgs";
     flake-utils.follows = "aleph2-common/flake-utils";
     nix-ros-overlay.follows = "aleph2-common/nix-ros-overlay";
-    aleph2-common.url =
-      "git+https://gitlab.continuum.ii.uni.wroc.pl/continuum/software/aleph2_common?ref=jazzy";
+    aleph2-common.url = "git+https://gitlab.continuum.ii.uni.wroc.pl/continuum/software/aleph2_common";
   };
-  outputs = { self, nixpkgs, flake-utils, nix-ros-overlay, aleph2-common }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      nix-ros-overlay,
+      aleph2-common,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = (import nixpkgs {
-          system = system;
-          overlays = [ ];
-        }).pkgs;
-        ros = (import nixpkgs {
-          system = system;
-          overlays = [ nix-ros-overlay.overlays.default ];
-        }).pkgs.rosPackages.jazzy;
+        pkgs =
+          (import nixpkgs {
+            system = system;
+            overlays = [ ];
+          }).pkgs;
+        ros =
+          (import nixpkgs {
+            system = system;
+            overlays = [ nix-ros-overlay.overlays.default ];
+          }).pkgs.rosPackages.jazzy;
 
-        aleph2-description =
-          aleph2-common.packages.${system}.aleph2-description;
+        aleph2-description = aleph2-common.packages.${system}.aleph2-description;
         input-manager = aleph2-common.packages.${system}.input-manager;
 
-        aleph2-gui =
-          ros.callPackage (import ./aleph2_gui) { inherit input-manager; };
-        aleph2-gz =
-          ros.callPackage (import ./aleph2_gz) { inherit aleph2-description; };
-        aleph2-viz =
-          ros.callPackage (import ./aleph2_viz) { inherit aleph2-description; };
+        aleph2-gui = ros.callPackage (import ./aleph2_gui) { inherit input-manager; };
+        aleph2-gz = ros.callPackage (import ./aleph2_gz) { inherit aleph2-description; };
+        aleph2-viz = ros.callPackage (import ./aleph2_viz) { inherit aleph2-description; };
 
-      in {
+      in
+      {
         packages = {
           inherit aleph2-gui aleph2-gz aleph2-viz;
           default = aleph2-viz;
@@ -37,11 +43,16 @@
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = [
             (ros.buildEnv {
-              paths = [ ros.ros-core aleph2-gui aleph2-gz aleph2-viz ];
+              paths = [
+                ros.ros-core
+                aleph2-gui
+                aleph2-gz
+                aleph2-viz
+              ];
             })
           ];
         };
         formatter = pkgs.nixfmt;
-      });
+      }
+    );
 }
-
